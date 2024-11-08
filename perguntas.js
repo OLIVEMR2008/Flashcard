f6954aa94f6197ba8a48b65272d15aa6d2161cc9
OLIVEMR2008 <149404368+OLIVEMR2008@users.noreply.github.com>
2024-11-08 08:20:49 -0300

    .1

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Geral',
+    'O que é Watanagashi?',
+    'O festival anual da vila de Hinamizawa, que acontece em Junho.'
+)
+
+
+criaCartao(
+    'Específica',
+    'O que é a Síndrome de Hinamizawa?',
+    'Uma doença/droga que faz o usuário ficar a beira da insanidade.'
+)
+
+
+criaCartao(
+    'Umineko',
+    'A Bruxa Bernkastel é a Rika Furude?',
+    'Indiretamente sim. Uma variação da Rika vindo dessa realidade, que virou uma Bruxa com poder dos desejos.'
+)
+
+
+criaCartao(
+    'Umineko',
+    'O que são as Bruxas (Witches)?',
+    'São humanos meta-transcendentais, que transcendem acima da vida e a morte.'
+)
+
