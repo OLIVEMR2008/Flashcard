b6e32a53200522997ddac4aa82d30ed7b5d97b1e
OLIVEMR2008 <149404368+OLIVEMR2008@users.noreply.github.com>
2024-11-08 08:34:53 -0300

    .1

--- perguntas.js
+++ perguntas.js
@@ -36,3 +36,28 @@ criaCartao(
     'O que é a Deusa Hanyuu "Furude"?',
     'Uma Deusa meta-transcendental com o objetivo de manter a Rika Furude viva por todos os Fragmentos.'
 )
+
+criaCartao(
+    'Específica',
+    'O que são os Loopers ("Os que Passam por Repetições")?',
+    'São humanos com as benças de seus Deuses para terem imortalidade, vivendo de Fragmento para Fragmento.'
+)
+
+
+criaCartao(
+    'Específica',
+    'Quem teve a morte mais brutal em Higurashi?',
+    'A morte mais brutal da franquia vai para a Rika Furude, que está cheia de mortes extremamente grotescas e pesadas em toda a obra. Sendo quando ela se esfaqueou até a morte, ou quando foi estripada pela sua melhor amiga Satoko Houjou.'
+)
+
+criaCartao(
+    'Específica',
+    'Qual a temporada mais violenta do Anime de Higurashi?',
+    'A temporada mais violenta do Anime é debatível entre a primeira temporada (Studio DEEN) ou as duas últimas (Studio Passione). Por causa do estilo de gore diferente entre elas.'
+)
+
+criaCartao(
+    'Específica',
+    'Por quanto tempo a Rika Furude está passando por Repetições?',
+    'Debatívelmente mais de 1000 anos de Repetições, contando as mortes não muito contadas pela obra.'
+)
